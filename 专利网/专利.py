fa5f5f66f655f170ffe0a170f3c9aad11ca80929
LinWeiTao <18665448472@sina.cn>
2025-02-07 17:14:58 +0800

    update

---
+++ 专利网/专利.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+data = {
+
+}
+
+
+def create_driver():
+    chrome_driver_path = '../driver/chromedriver_119.exe'
+    option = webdriver.ChromeOptions()
+    option.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(executable_path=chrome_driver_path, chrome_options=option)
+    driver.implicitly_wait(.5)
+    driver.maximize_window()
+    return driver
+
+
+if __name__ == "__main__":
+    # 创建driver
+    driver = create_driver()
+    driver.get('https://iptop.lotut.com/')
+    print()
